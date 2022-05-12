712a1dfa8606109cbd3bb37db77b77024a6d03ab
fstipo <fstipanov@gmail.com>
2022-05-12 16:18:56 +0200

    add routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+  Link,
+  Outlet,
+  useParams,
+} from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="p-5">Home Route</h1>
+    </div>
+  );
+};
+
+const Learn = () => {
+  return (
+    <div>
+      <h1 className="p-5">Learn</h1>
+      <h4 className="p-3">All courses are listed here</h4>
+      <Link to="/learn/courses" className="btn btn-success mx-4">
+        courses
+      </Link>
+      <Link to="/learn/bundles" className="btn btn-warning">
+        bundle
+      </Link>
+      <Outlet />
+    </div>
+  );
+};
+
+const Courses = () => {
+  return (
+    <div>
+      <p className="p-3">Courses List</p>
+      <p className="px-3">Courses Card</p>
+      <Outlet />
+    </div>
+  );
+};
+
+const Bundles = () => {
+  return (
+    <div>
+      <p className="p-3">Bundles List</p>
+      <p className="px-3">Bundles Card</p>
+    </div>
+  );
+};
+
+const CourseID = () => {
+  const { courseID } = useParams();
+  return (
+    <div>
+      <p className="p-3">Course ID: {courseID} </p>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/myapps" element={<Navigate to="/learn" />} />
+        <Route path="/learn" element={<Learn />}>
+          <Route path="courses" element={<Courses />}>
+            <Route path=":courseID" element={<CourseID />} />
+          </Route>
+          <Route path="bundles" element={<Bundles />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
